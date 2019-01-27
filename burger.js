a799ae8231b599a0c84cf5673f871abf6a0642b0
Ggoings <42050294+Ggoings@users.noreply.github.com>
2019-01-27 07:40:23 -0600

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,4 @@
+The user may enter any burger name to add it to the menu. 
+This also adds the new burger entry into the MySQL database. 
+The initial burger entry is added as available on the menu and placed on the screen.
+The user may then eat any burger by clicking on it, which moves it into the database.
